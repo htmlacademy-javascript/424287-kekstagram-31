bb46232899e3f87a0263d1315effe92974b55f7a
Keks <keks@htmlacademy.ru>
2024-02-29 16:17:52 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/functions.js
@@ -0,0 +1,33 @@
+//Функция для проверки длины строки
+
+const stringLength = (string, maxLength) => {
+  if (string.length <= maxLength) {
+    return true;
+  }
+  return false;
+};
+
+stringLength('стрjj', 4);
+stringLength('проверяемая строка', 20);
+stringLength('проверяемая строка', 18);
+stringLength('проверяемая строка', 10);
+
+// Палиндром или нет
+const definitionOfPalindrom = (string) => {
+  string = string.replaceAll(' ', '');
+
+  const modifiedString = string.toLowerCase();
+  let newString = '';
+  for (let i = modifiedString.length - 1; i >= 0; i--) {
+    newString += modifiedString[i];
+
+  }
+
+  return modifiedString === newString ? 'true' : 'false';
+
+};
+definitionOfPalindrom('Лёша на полке клопа нашёл ');
+
+//Извлекаем цифры
+
+
---
+++ 5/js/util.js
@@ -0,0 +1,27 @@
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createRandomId = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+const randomElement = (elem) => elem[getRandomInteger(0, elem.length - 1)];
+export {randomElement};
+export {getRandomInteger};
+export {createRandomId};
