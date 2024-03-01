868ba54f50375c8ce108aa51552e5c599cfdb520
Надежда Шутько <shutko_bstu@mail.ru>
2024-03-01 17:22:50 +0300

    Проверит, проходит ли встреча в течение рабочего дня

--- js/functions.js
+++ js/functions.js
@@ -28,6 +28,37 @@ const definitionOfPalindrom = (string) => {
 };
 definitionOfPalindrom('Лёша на полке клопа нашёл ');
 
-//Извлекаем цифры
+// функция Делу — время
+
+/*
+'8:00' - начало рабочего дня
+'17:30' - конец рабочего дня
+'14:00' - начало встречи
+90 - продолжительность встречи в минутах
+*/
+
+
+const convertToMinutes = (el) => {
+  const time = el.split(':').map((elem) => parseInt(elem,10));
+  time[0] *= 60;
+
+  const getMinutes = time.reduce((sum, element) => sum + element, 0);
+
+  return getMinutes;
+};
+const isInDay = (startOfDay,endOfDay,startOfMeeting,durationOfMeeting) => {
+  const timeOfStart = convertToMinutes(startOfDay);
+  const timeOfEnd = convertToMinutes(endOfDay);
+  const timeOfStartMeeting = convertToMinutes(startOfMeeting);
+  const timeOfMeeting = timeOfStartMeeting + durationOfMeeting;
+
+  return (timeOfMeeting <= timeOfEnd && timeOfStartMeeting >= timeOfStart);
+
+};
+isInDay('08:00', '17:30', '14:00', 90);
+isInDay('8:0', '10:0', '8:0', 120);
+isInDay('08:00', '14:30', '14:00', 90);
+isInDay('14:00', '17:30', '08:0', 90);
+isInDay('8:00', '17:30', '08:00', 900);
 
 
