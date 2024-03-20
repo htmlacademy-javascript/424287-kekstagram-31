92af096f47a23497831dff9c0cfc46b966226e0d
Надежда Шутько <shutko_bstu@mail.ru>
2024-03-20 19:52:28 +0300

    Изменяет масштаб фото

--- js/photo-resize.js
+++ js/photo-resize.js
@@ -2,34 +2,31 @@ const btnSmaller = document.querySelector('.scale__control--smaller');
 const btnBigger = document.querySelector('.scale__control--bigger');
 const scaleValue = document.querySelector('.scale__control--value');
 const imageContainer = document.querySelector('.img-upload__preview');
-const image = imageContainer.querySelector('img');
 const inputValue = document.querySelector('.scale__control--value');
 const STEP = 25;
 const minValue = 25;
 const maxValue = 100;
-scaleValue.value = 100;
-let value = parseInt(scaleValue.value,10);
+let value = scaleValue.value.replace('%', ' ');
 
 btnSmaller.addEventListener('click', () => {
 
-  if (scaleValue.value > minValue) {
+  if (value > minValue) {
 
     value -= STEP;
 
-    image.style.transform = `scale(${value / 100})`;
-    scaleValue.value = value;
-    // scaleValue.value = `${value}%`;
+    imageContainer.style.transform = `scale(${value / 100})`;
     inputValue.value = value;
+    scaleValue.value = `${value}%`;
   }
 });
 btnBigger.addEventListener('click', () => {
 
-  if (scaleValue.value < maxValue) {
+  if (value < maxValue) {
     value += STEP;
-    image.style.transform = `scale(${value / 100})`;
-    scaleValue.value = value;
+    imageContainer.style.transform = `scale(${value / 100})`;
     inputValue.value = value;
+    scaleValue.value = `${value}%`;
 
   }
 });
-export {image};
+export {imageContainer};
