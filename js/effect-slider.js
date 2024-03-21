1be44ba22c20d7482b1672cc8780918f04d1128e
Надежда Шутько <shutko_bstu@mail.ru>
2024-03-21 15:42:38 +0300

    Применяет фильтры к изображению

--- js/effect-slider.js
+++ js/effect-slider.js
@@ -1,9 +1,9 @@
-import {image} from './resize-photo';
+import {imageContainer} from './photo-resize';
 const sliderElement = document.querySelector('.effect-level__slider');
 const effectLevelContainer = document.querySelector('.img-upload__effect-level');
-const effectValueInput = document.querySelector('.effect-level__value');
-const effectItem = document.querySelectorAll('.effects__item');
-effectValueInput.value = 0;
+const effectList = document.querySelector('.effects__list');
+const valueElement = document.querySelector('.effect-level__value');
+
 
 noUiSlider.create(sliderElement, {
   range: {
@@ -15,64 +15,80 @@ noUiSlider.create(sliderElement, {
   connect: 'lower',
 });
 
-sliderElement.noUiSlider.on('update', () => {
-  effectValueInput.value = sliderElement.noUiSlider.get();
+const EFFECTS = [
+  {
+    name: 'chrome',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    style: 'grayscale',
+    unit: ''
+  },
+  {
+    name: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    style: 'sepia',
+    unit: ''
+  },
+  {name: 'marvin',
+    min: 0,
+    max: 100,
+    step: 1,
+    style: 'invert',
+    unit: '%'
+  },
+  {name: 'phobos',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    style: 'blur',
+    unit: 'px'
+  },
+  {name:'heat',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    style: 'brightness',
+    unit: ''
+  },
+  {
+    name: 'none',
+    min: 0,
+    max: 1,
+    style: 'none',
+    unit: ''
+  }
+];
+effectLevelContainer.classList.add('hidden');
+
+effectList.addEventListener('change', (evt) => {
+
+  const selectedEffect = EFFECTS.find((effect) => effect.name === evt.target.value);
+
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: selectedEffect.min,
+      max: selectedEffect.max
+    },
+    step: selectedEffect.step,
+    start: selectedEffect.min
+  });
+  sliderElement.noUiSlider.on('update', () => {
+    valueElement.value = sliderElement.noUiSlider.get();
+    imageContainer.style.filter = `${selectedEffect.style}(${valueElement.value}${selectedEffect.unit})`;
+    if(selectedEffect.name === 'none') {
+      effectLevelContainer.classList.add('hidden');
+      imageContainer.style.filter = `${selectedEffect.style}`;
+
+    } else {
+      effectLevelContainer.classList.remove('hidden');
+
+    }
+  });
+
 
 });
 
-for (let i = 0; i < effectItem.length;i++) {
-  effectItem[i].addEventListener('click', () => {
-    switch (i) {
-      case 1:
-        effectLevelContainer.classList.remove('hidden');
 
-        image.style.filter = `scale(${effectValueInput.value})`;
-        break;
-      case 2:
-        effectLevelContainer.classList.remove('hidden');
-
-        image.style.filter = `sepia(${effectValueInput.value})`;
-        break;
-      case 3:
-        effectLevelContainer.classList.remove('hidden');
-
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 100,
-          },
-          step: 1,
-        });
-        image.style.filter = `invert(${`${effectValueInput.value}%`})`;
-        break;
-      case 4:
-        effectLevelContainer.classList.remove('hidden');
-
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3,
-          },
-          step: 0.1,
-        });
-        image.style.filter = `blur(${`${effectValueInput.value}px`})`;
-        break;
-      case 5:
-        effectLevelContainer.classList.remove('hidden');
-
-        sliderElement.noUiSlider.updateOptions({
-          range: {
-            min: 1,
-            max: 3,
-          },
-          step: 0.1,
-        });
-        image.style.filter = `brightness(${effectValueInput.value})`;
-        break;
-      default:
-        image.style.filter = 'unset';
-        effectLevelContainer.classList.add('hidden');
-    }
-
-  });
-}
