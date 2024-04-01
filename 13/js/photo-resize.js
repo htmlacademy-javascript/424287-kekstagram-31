9f8c9f3e169fa5d5ac75944a0329365ee026b1e1
Keks <keks@htmlacademy.ru>
2024-04-01 12:26:57 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/photo-resize.js
@@ -0,0 +1,35 @@
+const btnSmaller = document.querySelector('.scale__control--smaller');
+const btnBigger = document.querySelector('.scale__control--bigger');
+const scaleValue = document.querySelector('.scale__control--value');
+const imageContainer = document.querySelector('.img-upload__preview');
+const inputValue = document.querySelector('.scale__control--value');
+const STEP = 25;
+const minValue = 25;
+const maxValue = 100;
+// let value = scaleValue.value.replace('%', ' ');
+
+btnSmaller.addEventListener('click', () => {
+  let value = scaleValue.value.replace('%', ' ');
+
+  if (value > minValue) {
+
+    value -= STEP;
+
+    imageContainer.style.transform = `scale(${value / 100})`;
+    inputValue.value = value;
+    scaleValue.value = `${value}%`;
+
+  }
+});
+btnBigger.addEventListener('click', () => {
+  let value = scaleValue.value.replace('%', ' ');
+
+  if (value < maxValue) {
+    value += STEP;
+    imageContainer.style.transform = `scale(${value / 100})`;
+    inputValue.value = value;
+    scaleValue.value = `${value}%`;
+
+  }
+});
+export {imageContainer,scaleValue};
---
+++ 13/js/util.js
@@ -0,0 +1,79 @@
+const ALERT_SHOW_TIME = 5000;
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createRandomId = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= max - min + 1) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+const randomElement = (elem) => elem[getRandomInteger(0, elem.length - 1)];
+const showErrorDataMessage = () => {
+  const errorMessageTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+  const errorMessageFragment = document.createDocumentFragment();
+  const message = errorMessageTemplate.cloneNode(true);
+  errorMessageFragment.appendChild(message);
+  document.body.appendChild(errorMessageFragment);
+  setTimeout(() => {
+    message.classList.add('hidden');
+  }, ALERT_SHOW_TIME);
+};
+
+const createErrorMessage = () => {
+  const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+  const errorMessageFragment = document.createDocumentFragment();
+  const message = errorMessageTemplate.cloneNode(true);
+  errorMessageFragment.appendChild(message);
+  document.body.appendChild(errorMessageFragment);
+  document.querySelector('.error').classList.add('hidden');
+};
+const onDocumentKeydown = (evt) => {
+  if(evt.key === 'Escape') {
+    evt.preventDefault();
+    document.querySelector('.error').classList.add('hidden');
+  }
+};
+
+const showErrorMessage = () => {
+  document.querySelector('.error').classList.remove('hidden');
+
+  const errorBlock = document.querySelector('.error');
+  const closeModalBtn = errorBlock.querySelector('.error__button');
+  closeModalBtn.addEventListener('click', () => {
+    document.querySelector('.error').classList.add('hidden');
+    document.querySelector('.error').addEventListener('click', () => {
+      document.querySelector('.error').classList.add('hidden');
+      onDocumentKeydown();
+
+    });
+
+  });
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+export {randomElement};
+export {getRandomInteger};
+export {createRandomId};
+export {showErrorDataMessage,showErrorMessage, createErrorMessage,debounce};
