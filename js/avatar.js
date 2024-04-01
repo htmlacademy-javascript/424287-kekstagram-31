4914c89883a55bdfc076d8cb64a45cfe9aa5ff07
Надежда Шутько <shutko_bstu@mail.ru>
2024-04-01 12:44:26 +0300

    Подгружает фото

--- js/avatar.js
+++ js/avatar.js
@@ -7,7 +7,6 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
-
   if (matches) {
     preview.src = URL.createObjectURL(file);
 
